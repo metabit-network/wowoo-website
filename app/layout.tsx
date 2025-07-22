8227285094c7a21261e08ea8f1111af3d15e3d45
fazilah <fazilahramli@metabit.network>
2025-07-22 14:12:42 +0800

    test: test icon

--- app/layout.tsx
+++ app/layout.tsx
@@ -64,7 +64,7 @@ export async function generateMetadata(): Promise<Metadata> {
       siteName: "Wowoo",
       images: [
         {
-          url: "/favicon.ico",
+          url: "/image.png",
           width: 1200,
           height: 630,
           alt: "Wowoo Token",
